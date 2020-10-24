353386ac2162db9d94df052b4e796788b03ebfd6
Krausers <semenchenkov200510@gmail.com>
2020-10-24 14:58:53 +0400

    mod script file

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@ let money = prompt("Ваш бюджет на месяц?"),
 
 let appData = {
     budget: money,
+    dataTime: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
-    timeData: time,
     savings: false
 };
-    
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a2 = prompt("Во сколько обойдется?", ''),
-    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a4 = prompt("Во сколько обойдется?", '');
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце");
+let a2 = prompt("Во сколько обойдется?");
+let a3 = prompt("Введите обязательную статью расходов в этом месяце");
+let a4 = prompt("Во сколько обойдется?");
 
 appData.expenses.a1 = a2;
 appData.expenses.a3 = a4;
 
-alert(appData.budget / 30);
+alert("Ваш бюджет на 1 день: " + appData.budget / 30);
